af8a49d897e475b24642995c1d9bff92cb76fd60
dianrahmaji <dianrahmaji@gmail.com>
2022-03-13 20:34:38 +0700

    registered user routes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
+
+import userRoute from './routes/userRoute.js'
 
 dotenv.config()
 
@@ -18,6 +21,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(notFound)
+app.use(errorHandler)
 
 // routes
 app.get('/', (req, res) => {
@@ -25,5 +30,7 @@ app.get('/', (req, res) => {
     message: 'Hi mom!'
   })
 })
+
+app.use('/api/users', userRoute)
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`.yellow.bold))
