fa26a79cc364c17df76a20b17fde48c5b71f2279
dianrahmaji <dianrahmaji@gmail.com>
2022-07-13 10:02:26 +0700

    chore: added approveTeam controller

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -2,8 +2,8 @@ import asyncHandler from 'express-async-handler'
 import Repository from '../models/repositoryModel.js'
 import Team from '../models/teamModel.js'
 
-// @desc Create Repository
-// @route POST /api/repository
+// @desc Create Team
+// @route POST /api/team
 // @access Private/User
 const createTeam = asyncHandler(async (req, res) => {
   const { teamName: name, ...data } = req.body
@@ -18,4 +18,20 @@ const createTeam = asyncHandler(async (req, res) => {
   res.status(201).json(repository)
 })
 
-export { createTeam }
+// @desc Approve Team
+// @route PUT /api/team/:id/approve
+// @access Private/Admin
+const approveTeam = asyncHandler(async (req, res) => {
+  const team = await Team.findById(req.params.id)
+
+  if (!team) {
+    res.status(404)
+    throw new Error('Team not found')
+  }
+
+  team.isApproved = true
+  const approvedTeam = team.save()
+  res.status(200).json(approvedTeam)
+})
+
+export { createTeam, approveTeam }
