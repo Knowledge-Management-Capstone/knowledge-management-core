dc7ef7fbcfc5719431ac79d5d341410c83e6b86f
dianrahmaji <dianrahmaji@gmail.com>
2022-07-21 22:07:34 +0700

    feat: added data destroyer

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import Document from '../models/documentModel.js'
-import Folder from '../models/folderModel'
+import Folder from '../models/folderModel.js'
 import Message from '../models/messageModel.js'
 import Repository from '../models/repositoryModel.js'
 import Team from '../models/teamModel.js'
