fc71a76c151b662e18988184f8437db6d8d26582
dianrahmaji <dianrahmaji@gmail.com>
2022-07-21 14:23:16 +0700

    feat: using Promise.all

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -7,12 +7,14 @@ import Team from '../models/teamModel.js'
 import User from '../models/userModel.js'
 
 const reset = asyncHandler(async (req, res) => {
-  await Document.deleteMany({})
-  await Folder.deleteMany({})
-  await Message.deleteMany({})
-  await Repository.deleteMany({})
-  await Team.deleteMany({})
-  await User.deleteMany({})
+  await Promise.all([
+    Document.deleteMany({}),
+    Folder.deleteMany({}),
+    Message.deleteMany({}),
+    Repository.deleteMany({}),
+    Team.deleteMany({}),
+    User.deleteMany({})
+  ])
 
   res.status(204).json({ message: 'Successfully reset database' })
 })
