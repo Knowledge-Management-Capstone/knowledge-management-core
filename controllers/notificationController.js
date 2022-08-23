f27dcb83a654616c97cbe525920167a3044ddebf
dianrahmaji <dianrahmaji@gmail.com>
2022-08-23 14:34:25 +0700

    feat: added unread_messages query

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,11 +1,47 @@
 import asyncHandler from "express-async-handler";
+import mongoose from "mongoose";
+import Chat from "../models/chatModel.js";
 
 /**
  * @desc Get Notifications By MemberId and RoomId
  * @route GET /api/notification/:roomId/member/:memberId
  * @access Private/User
  */
-const getNotificationById = asyncHandler(async () => {});
+const getNotificationById = asyncHandler(async (req, res) => {
+  const { roomId, memberId } = req.params;
+
+  const chat = await Chat.aggregate([
+    { $match: { _id: mongoose.Types.ObjectId(roomId) } },
+    {
+      $lookup: {
+        from: "messages",
+        localField: "messages",
+        foreignField: "_id",
+        as: "messages",
+      },
+    },
+    { $project: { _id: 0, messages: 1 } },
+    { $unwind: "$messages" },
+    {
+      $replaceRoot: { newRoot: "$messages" },
+    },
+    {
+      $match: {
+        $and: [
+          { sender: { $ne: mongoose.Types.ObjectId(memberId) } },
+          { readBy: { $nin: [mongoose.Types.ObjectId(memberId)] } },
+        ],
+      },
+    },
+    { $count: "unread_messages" },
+  ]).exec();
+
+  const response = {
+    unread_messages: chat[0]?.unread_messages || 0,
+  };
+
+  res.status(200).json(response);
+});
 
 /**
  * @desc Reset Notification By MemberId and RoomId
